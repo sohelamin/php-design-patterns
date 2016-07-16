219d4a58e8099c2297fcab80c047ee35415c8278
Sohel Amin <sohelamincse@gmail.com>
2016-07-16 19:24:41 +0600

    Singleton pattern added

---
+++ singleton.php
@@ -0,0 +1,29 @@
+<?php
+
+class Singleton
+{
+    private static $instance;
+
+    public static function getInstance()
+    {
+        if (null === self::$instance) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    public function __construct()
+    {
+
+    }
+
+    public function sayHi()
+    {
+        echo 'Hi';
+    }
+}
+
+$singleton = Singleton::getInstance();
+
+$singleton->sayHi();
